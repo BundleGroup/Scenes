575993e935bdd2c414a7cbfaa2a852424c47cc76
56738 <git@56738.me>
2025-06-22 14:23:47 +0200

    Add maven-publish and GitHub Actions

--- bundlescenes-api/build.gradle.kts
+++ bundlescenes-api/build.gradle.kts
@@ -1,8 +1,29 @@
 plugins {
     id("bundlescenes.base")
+    id("maven-publish")
 }
 
 dependencies {
     compileOnly(libs.paper.api)
     compileOnlyApi(libs.bundleentities.api)
+}
+
+publishing {
+    repositories {
+        maven("https://repo.theatlas.gg/repository/maven-snapshots/") {
+            name = "atlas"
+            credentials(PasswordCredentials::class)
+        }
+    }
+
+    publications {
+        register<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
 }
